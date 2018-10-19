50fb183b15398bc9ad34e638fc4e8e9e14008d0d
Nate Scherer <376408+natescherer@users.noreply.github.com>
2018-10-19 15:58:27 -0400

    Add dynamic version numbering

--- appveyorbuild.ps1
+++ appveyorbuild.ps1
@@ -3,22 +3,16 @@ Write-Host -Object ''
 
 # Make sure we're using the Master branch and that it's not a pull request
 # Environmental Variables Guide: https://www.appveyor.com/docs/environment-variables/
-if ($env:APPVEYOR_REPO_BRANCH -ne 'master') 
-{
+if ($env:APPVEYOR_REPO_BRANCH -ne 'master') {
     Write-Warning -Message "Skipping version increment and publish for branch $env:APPVEYOR_REPO_BRANCH"
-}
-elseif ($env:APPVEYOR_PULL_REQUEST_NUMBER -gt 0)
-{
+} elseif ($env:APPVEYOR_PULL_REQUEST_NUMBER -gt 0) {
     Write-Warning -Message "Skipping version increment and publish for pull request #$env:APPVEYOR_PULL_REQUEST_NUMBER"
-}
-else
-{
+} else {
     # We're going to add 1 to the revision value since a new commit has been merged to Master
     # This means that the major / minor / build values will be consistent across GitHub and the Gallery
-    Try 
-    {
+    try {
         # This is where the module manifest lives
-        $manifestPath = '.\src\Rubrik.psd1'
+        $manifestPath = ".\src\$($env:APPVEYOR_PROJECT_NAME).psd1"
 
         # Start by importing the manifest to determine the version, then add 1 to the revision
         $manifest = Test-ModuleManifest -Path $manifestPath
@@ -41,8 +35,7 @@ else
         (Get-Content -Path $manifestPath) -replace 'FunctionsToExport = ', 'FunctionsToExport = @(' | Set-Content -Path $manifestPath -Force
         (Get-Content -Path $manifestPath) -replace "$($functionList[-1])'", "$($functionList[-1])')" | Set-Content -Path $manifestPath -Force
     }
-    catch
-    {
+    catch {
         throw $_
     }
 
@@ -55,7 +48,7 @@ else
     Write-Host -Object ''
 
     # Publish the new version to the PowerShell Gallery
-    Try {
+    try {
         # Build a splat containing the required details and make sure to Stop for errors which will trigger the catch
         $PM = @{
             Path        = '.\Rubrik'
