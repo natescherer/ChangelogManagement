93baac75b9955abacff7289af14c2596bb4d3525
Nate Scherer <376408+natescherer@users.noreply.github.com>
2019-02-07 15:20:21 -0500

    Add missing qutoes

--- test/ChangelogManagement.Tests.ps1
+++ test/ChangelogManagement.Tests.ps1
@@ -667,7 +667,7 @@ InModuleScope $ModuleName {
                 "$Eol" +
                 "The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).$Eol" +
                 "$Eol" +
-                "## [Unreleased]$Eol)
+                "## [Unreleased]$Eol")
         }
     }
 
